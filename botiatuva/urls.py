49e1d26f93825c6a17eb8bde103a8c67a9b5fbbf
joao-vitor-vieira-radacki <70207744+joao-vitor-viera-rodacki@users.noreply.github.com>
2022-05-10 12:41:38 -0300

    Adicinando imagens a API rest.

--- botiatuva/urls.py
+++ botiatuva/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from posixpath import basename
+import django
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 from rest_framework import routers
 from materiais.api import viewsets as Materiaisviewsets
@@ -28,4 +32,4 @@ route.register(r'material/',Materiaisviewsets.MateriaisViewsets, basename="mater
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(route.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- materiais/models.py
+++ materiais/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from uuid import uuid4
+
+
+def  upload_image(intance,filename):
+  return f"{intance.id_material}-{filename}"
 
 class materiais(models.Model):
+  id_material = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   nome = models.CharField(max_length=100)
   quantidade = models.IntegerField()
   preco = models.IntegerField()
+  image = models.ImageField(upload_to=upload_image, blank=True, null=True)
   
