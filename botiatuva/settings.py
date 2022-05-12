9bc069ec60785cf972b207a5bf575480f34f0dd2
joao-vitor-vieira-radacki <70207744+joao-vitor-viera-rodacki@users.noreply.github.com>
2022-05-12 19:16:58 -0300

    Pronto para deploy

--- botiatuva/settings.py
+++ botiatuva/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +128,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'midia')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
